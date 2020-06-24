54f3d91016f4591a40ba6014e5e82da3098329f0
jash2001-art <63535402+jash2001-art@users.noreply.github.com>
2020-06-24 17:17:30 +0530

    Crested on Java 8.0
    Created by Jash Patel

---
+++ switchcase.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+package switchcase;
+public class switchcase{
+	public static void main(string[] args) {
+		int choice;
+		system.out.println("Pick one: 1. Hi\t 2. Hey\t 3. Hello\t");
+		Scanner s= new Scanner(system.in);
+		choice= s.nextint();
+		switch(choice)
+		{
+			case 1: system.out.println("You said Hi.");
+			break;
+			case 2: system.out.println("You said Hey.");
+			break;
+			case 3: system.out.println("You said Hello.");
+			default: system.out.println("Invalid case.");
+			}
+			}
+			}
+			
+			
+		
+		
